4fe7f8a60c1cfa947500fe9398fcbed39ed57ecf
nitod2001 <dotinvta2@gmail.com>
2026-07-27 12:34:50 +0700

    feat: add homepage challenge chapter

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import { ChallengeSection } from "@/components/sections/home/challenge-section";
 import { HeroSection } from "@/components/sections/home/hero-section";
 
 type Locale = "en" | "vi";
@@ -9,5 +10,10 @@ export default async function LocaleHomePage({
 }>) {
   const { locale } = await params;
 
-  return <HeroSection locale={locale as Locale} />;
+  return (
+    <>
+      <HeroSection locale={locale as Locale} />
+      <ChallengeSection locale={locale as Locale} />
+    </>
+  );
 }
